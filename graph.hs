49f46e809fed4f31d705684ebf54d727d0c7d0d2
Charlie Burrows <charlie.burrows@gmail.com>
2014-09-13 11:11:55 -0400

    windows_to_graph is complete

--- graph.hs
+++ graph.hs
@@ -1,15 +1,26 @@
 -- List of error codes -> a window
 data Edge = Edge Integer Integer Integer deriving Show
+type Graph = [Edge]
 --data Path = [Ord]
 
-inc_edge :: [Edge] -> Integer -> Integer -> [Edge]
+inc_edge :: Graph -> Integer -> Integer -> Graph 
 inc_edge [] x y = [Edge x y 1]
 inc_edge (g@(Edge a b n):gs) x y
     | (a == x && b == y) || (a == x && b == y) = Edge a b (n+1):gs
     | otherwise = (Edge a b n: (inc_edge gs x y))
---window_to_edges :: [[Ord]] -> Graph
---window_to_edges [] = []
---window_to_egdes (w:ws) = 
+
+rmdups :: [Integer] -> [Integer]
+rmdups (x:xs)
+  | xs == [] = [x]
+  | elem x xs = rmdups xs
+  | otherwise = x:(rmdups xs)
+
+pairs :: [Integer] -> [(Integer, Integer)]
+pairs [x] = []
+pairs (x:xs) = map (\ y -> (x,y)) xs ++ pairs xs
+
+windows_to_graph :: [[Integer]] -> Graph
+windows_to_graph ws = foldl (\ graph (x, y) -> inc_edge graph x y) [] $ concatMap (pairs . rmdups) ws
 
 --children :: Graph -> Ord -> Graph
 --shortest_path :: Graph -> Ord -> Ord -> Path
